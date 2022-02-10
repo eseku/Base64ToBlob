66d244c30f7ca2224136e70eba5fc5279f1dd2df
Eseku <40706339+eustacequartey@users.noreply.github.com>
2022-02-10 18:28:57 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var url = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+
+fetch(url)
+.then(res => res.blob())
+.then(console.log)
